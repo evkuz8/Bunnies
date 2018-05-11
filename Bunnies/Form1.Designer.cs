28fc2b6ab0bb260cc15ab3eac971d974796b9a43
evkuz8 <evgn.worker@gmail.com>
2018-05-11 17:21:21 +0500

    Revert "Элементы управления"
    This reverts commit 8db493e5d4164be75c4957751b2cc358526e4367.

--- Bunnies/Form1.Designer.cs
+++ Bunnies/Form1.Designer.cs
@@ -28,78 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.enterBtn = new System.Windows.Forms.Button();
-            this.loginLbl = new System.Windows.Forms.Label();
-            this.passwordLbl = new System.Windows.Forms.Label();
-            this.loginTxt = new System.Windows.Forms.TextBox();
-            this.passwordTxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // enterBtn
+            // button1
             // 
-            this.enterBtn.Location = new System.Drawing.Point(53, 127);
-            this.enterBtn.Name = "enterBtn";
-            this.enterBtn.Size = new System.Drawing.Size(145, 23);
-            this.enterBtn.TabIndex = 0;
-            this.enterBtn.Text = "Enter!!!";
-            this.enterBtn.UseVisualStyleBackColor = true;
-            // 
-            // loginLbl
-            // 
-            this.loginLbl.AutoSize = true;
-            this.loginLbl.Location = new System.Drawing.Point(52, 27);
-            this.loginLbl.Name = "loginLbl";
-            this.loginLbl.Size = new System.Drawing.Size(36, 13);
-            this.loginLbl.TabIndex = 1;
-            this.loginLbl.Text = "Login:";
-            // 
-            // passwordLbl
-            // 
-            this.passwordLbl.AutoSize = true;
-            this.passwordLbl.Location = new System.Drawing.Point(32, 73);
-            this.passwordLbl.Name = "passwordLbl";
-            this.passwordLbl.Size = new System.Drawing.Size(56, 13);
-            this.passwordLbl.TabIndex = 2;
-            this.passwordLbl.Text = "Password:";
-            // 
-            // loginTxt
-            // 
-            this.loginTxt.Location = new System.Drawing.Point(106, 24);
-            this.loginTxt.Name = "loginTxt";
-            this.loginTxt.Size = new System.Drawing.Size(100, 20);
-            this.loginTxt.TabIndex = 3;
-            // 
-            // passwordTxt
-            // 
-            this.passwordTxt.Location = new System.Drawing.Point(106, 70);
-            this.passwordTxt.Name = "passwordTxt";
-            this.passwordTxt.Size = new System.Drawing.Size(100, 20);
-            this.passwordTxt.TabIndex = 4;
+            this.button1.Location = new System.Drawing.Point(350, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(237, 178);
-            this.Controls.Add(this.passwordTxt);
-            this.Controls.Add(this.loginTxt);
-            this.Controls.Add(this.passwordLbl);
-            this.Controls.Add(this.loginLbl);
-            this.Controls.Add(this.enterBtn);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button enterBtn;
-        private System.Windows.Forms.Label loginLbl;
-        private System.Windows.Forms.Label passwordLbl;
-        private System.Windows.Forms.TextBox loginTxt;
-        private System.Windows.Forms.TextBox passwordTxt;
+        private System.Windows.Forms.Button button1;
     }
 }
 
